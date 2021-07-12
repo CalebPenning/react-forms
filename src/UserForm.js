e137c663feb28e3b2fcbfb44506faf33775844d8
calebpenning <calebpenning@mail.com>
2021-07-12 14:46:35 -0700

    modified UserForm component

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,13 +1,66 @@
 import { useState } from "react"
 
 const UserForm = () => {
-    const handleChange = (e) => {
-        setUsername(e.target.value)
+
+    const initialState = {  
+        username: "",
+        email: "",
+        password: ""
     }
-    const [username, setUsername] = useState("testuser")
+
+    // // pieces of s**** (state)
+
+    const handleChange = e => {
+        //e.target.value and e.target.name is what we utilize for generic handlers
+        const { name, value } = e.target
+        setFormData(data => ({
+            ...data, 
+            [name]: value
+        }))
+    }
+
+    const [formData, setFormData] = useState(initialState)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const { username, email, password } = formData
+        alert(`Created user, ${username} with email ${email} & password ${password}`)
+        setFormData(initialState)
+    }
+
     return (
-        <form>
-            <input type="text" placeholder="username" value={username}  onChange={handleChange} />
+        <form onSubmit={handleSubmit} >
+
+            <label htmlFor="username">Username</label>
+            <input 
+                type="text" 
+                id="username"
+                name="username"
+                placeholder="username" 
+                value={formData.username}  
+                onChange={handleChange}  
+            />
+
+            <label htmlFor="email">Email</label>
+            <input 
+                type="email" 
+                placeholder="email" 
+                id="email"
+                name="email"
+                value={formData.email} 
+                onChange={handleChange}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input 
+                type="password"
+                placeholder="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+
             <button>Submit</button>
         </form>
     )
